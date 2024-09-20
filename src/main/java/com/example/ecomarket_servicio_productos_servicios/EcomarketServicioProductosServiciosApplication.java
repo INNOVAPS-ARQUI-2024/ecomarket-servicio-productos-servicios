c35da7ce07cc055f8d2fe90b1dbbc059899b4454
XxDaniel-AlzatexX <daniel-alzatem@javeriana.edu.co>
2024-09-20 01:30:42 -0500

    :feat-feat:Creación del proyecto base

---
+++ src/main/java/com/example/ecomarket_servicio_productos_servicios/EcomarketServicioProductosServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecomarket_servicio_productos_servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketServicioProductosServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketServicioProductosServiciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecomarket_servicio_productos_servicios/EcomarketServicioProductosServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomarket_servicio_productos_servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomarketServicioProductosServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
