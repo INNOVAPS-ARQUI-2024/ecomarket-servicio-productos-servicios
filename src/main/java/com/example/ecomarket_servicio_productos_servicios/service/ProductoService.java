7e47d99f35e9d84ed85ac6ec0828be75a7ef2b90
jesusdme <jdmolinae@hotmail.com>
2024-10-22 22:31:11 -0500

    :wrench: chore: atributos de productos actualizados

--- src/main/java/com/example/ecomarket_servicio_productos_servicios/service/ProductoService.java
+++ src/main/java/com/example/ecomarket_servicio_productos_servicios/service/ProductoService.java
@@ -1,10 +1,12 @@
 package com.example.ecomarket_servicio_productos_servicios.service;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.ecomarket_servicio_productos_servicios.entity.Producto;
 import com.example.ecomarket_servicio_productos_servicios.repository.ProductoRepository;
@@ -23,26 +25,47 @@ public class ProductoService {
         return productoRepository.findById(id).orElse(null);
     }
 
-    public Producto guardarProducto(Producto producto) {
+    public Producto guardarProducto(Producto producto, MultipartFile picture) {
         if (producto.getName() == null || producto.getPrice() <= 0 || producto.getCategory() == null) {
             throw new IllegalArgumentException("Los campos 'nombre', 'precio', y 'categoría' son obligatorios.");
         }
+
         producto.setCreatedAt(new Date());
+        producto.setSold(0); // Inicializamos el campo 'sold' a 0
+
+        if (picture != null && !picture.isEmpty()) {
+            try {
+                producto.setPicture(picture.getBytes()); // Guardamos la imagen como bytes
+            } catch (IOException e) {
+                throw new RuntimeException("Error al guardar la imagen", e);
+            }
+        }
+
         return productoRepository.save(producto);
     }
 
-    public Producto actualizarProducto(String id, Producto detallesProducto) {
+    public Producto actualizarProducto(String id, Producto detallesProducto, MultipartFile picture) {
         Producto producto = productoRepository.findById(id).orElse(null);
         if (producto != null) {
-            if (detallesProducto.getName() == null || detallesProducto.getPrice() <= 0
-                    || detallesProducto.getCategory() == null) {
+            if (detallesProducto.getName() == null || detallesProducto.getPrice() <= 0 || detallesProducto.getCategory() == null) {
                 throw new IllegalArgumentException("Los campos 'nombre', 'precio', y 'categoría' son obligatorios.");
             }
+
             producto.setName(detallesProducto.getName());
             producto.setDescription(detallesProducto.getDescription());
             producto.setPrice(detallesProducto.getPrice());
+            producto.setCurrency(detallesProducto.getCurrency());
             producto.setCategory(detallesProducto.getCategory());
             producto.setStock(detallesProducto.getStock());
+
+            if (picture != null && !picture.isEmpty()) {
+                try {
+                    producto.setPicture(picture.getBytes()); // Actualizamos la imagen si es necesario
+                } catch (IOException e) {
+                    throw new RuntimeException("Error al actualizar la imagen", e);
+                }
+            }
+
             return productoRepository.save(producto);
         }
         return null;
